bffac9fcf853751501ab36d9f914dd7b0b818bde
James Bridges <masterbridges@gmail.com>
2020-01-04 14:39:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,25 @@ class Dog
   def initialize(breed="Mutt")
     @breed = breed
   end
-
+  
+  def breed=(breed)
+      @breed = breed
+    end
+   
+    def breed
+      @breed
+    end
+    
   def initialize(name)
     @name = name
+  end
+
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
   end
 
   attr_accessor :breed
